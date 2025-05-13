861609ddc03e07869f9d8456f2aede2298008452
Gregory Schier <gschier1990@gmail.com>
2025-05-13 11:38:02 -0700

    Update encryption help

--- src-web/components/EncryptionHelp.tsx
+++ src-web/components/EncryptionHelp.tsx
@@ -1,14 +1,13 @@
-import {VStack} from "./core/Stacks";
+import { VStack } from './core/Stacks';
 
 export function EncryptionHelp() {
-    return <VStack space={3}>
-        <p>
-            Encrypt values like secrets and tokens. When enabled, Yaak will also encrypt HTTP responses,
-            cookies, and authentication credentials automatically.
-        </p>
-        <p>
-            Encrypted data remains secure when syncing to the filesystem or Git, and when exporting or
-            sharing with others.
-        </p>
+  return (
+    <VStack space={3}>
+      <p>Encrypt passwords, tokens, and other sensitive info when encryption is enabled.</p>
+      <p>
+        Encrypted data remains secure when syncing to the filesystem or Git, and when exporting or
+        sharing with others.
+      </p>
     </VStack>
+  );
 }
