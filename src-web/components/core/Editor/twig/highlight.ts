409620f5331c9823405a6173f2fb9959a0f407ad
Gregory Schier <gschier1990@gmail.com>
2025-05-19 13:37:12 -0700

    More advanced template grammar
    Fixes https://feedback.yaak.app/p/cannot-escape-call-to-variable-in-json-body

--- src-web/components/core/Editor/twig/highlight.ts
+++ src-web/components/core/Editor/twig/highlight.ts
@@ -1,7 +1,14 @@
 import { styleTags, tags as t } from '@lezer/highlight';
 
 export const highlight = styleTags({
-  TagOpen: t.tagName,
-  TagClose: t.tagName,
-  TagContent: t.keyword,
+  "${[": t.bracket,
+  "]}": t.bracket,
+  "(": t.bracket,
+  ")": t.bracket,
+  "=": t.bracket,
+  ",": t.bracket,
+  Tag: t.keyword,
+  Identifier: t.variableName,
+  ChainedIdentifier: t.variableName,
+  Boolean: t.bool,
 });
