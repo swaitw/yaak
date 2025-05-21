2e144f064da806091ad126ca9b55e96d538d7894
Gregory Schier <gschier1990@gmail.com>
2025-05-21 08:26:15 -0700

    Fix syntax highlighting

--- src-web/components/core/Editor/twig/extension.ts
+++ src-web/components/core/Editor/twig/extension.ts
@@ -72,7 +72,7 @@ function mixLanguage(base: LanguageSupport): LRLanguage {
 
       return {
         parser: base.language.parser,
-        overlay: (node) => node.type.name === 'PlainText',
+        overlay: (node) => node.type.name === 'Text',
       };
     }),
   });
