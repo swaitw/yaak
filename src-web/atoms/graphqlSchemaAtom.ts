a03ec8875c76df1ebf05648d8ee9c4de68df47bf
Gregory Schier <gschier1990@gmail.com>
2025-07-08 09:29:56 -0700

    Persist gql docs shown state

--- src-web/atoms/graphqlSchemaAtom.ts
+++ src-web/atoms/graphqlSchemaAtom.ts
@@ -1,3 +1,3 @@
-import { atom } from 'jotai';
+import { atomWithKVStorage } from '../lib/atoms/atomWithKVStorage';
 
-export const showGraphQLDocExplorerAtom = atom<boolean>(false);
+export const showGraphQLDocExplorerAtom = atomWithKVStorage<boolean>('show_graphql_docs', false);
